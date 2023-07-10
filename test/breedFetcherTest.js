cc64ef3577d41a2870436c9fed1d11ead99ad4aa
EstelleWho <122816900+EstelleWho@users.noreply.github.com>
2023-07-10 03:01:23 -0400

    add test

---
+++ test/breedFetcherTest.js
@@ -0,0 +1,27 @@
+// breedFetcherTest.js
+
+const { fetchBreedDescription } = require('../breedFetcher');
+const { assert } = require('chai');
+
+describe('fetchBreedDescription', () => {
+  it('returns a string description for a valid breed, via callback', (done) => {
+    fetchBreedDescription('Siberian', (err, desc) => {
+      // we expect no error for this scenario
+      assert.equal(err, null);
+
+      const expectedDesc = "The Siberians dog like temperament and affection makes the ideal lap cat and will live quite happily indoors. Very agile and powerful, the Siberian cat can easily leap and reach high places, including the tops of refrigerators and even doors.";
+
+      // compare returned description
+      assert.equal(expectedDesc, desc.trim());
+
+      done();
+    });
+  });
+  // Add another test (it) here which should test the scenario where an invalid/non-existent breed is passed in.
+  it('returns invalid/non-existent breed', (done) => {
+    fetchBreedDescription('Wolfers', (err, desc) => {
+      assert.equal(null, desc);
+      done();
+    });
+  });
+});
